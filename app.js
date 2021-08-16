3b8b5b9c19919cdffe379313b19f69d511aa0fbf
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-17 04:03:00 +0700

    API Folder Structure UPDATE

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(cors({ origin: "*" }));
+
+// const activitiesRoute = require("./routes/activitiesRoute"),
+
+// app.use("/api/ot", activitiesRoute);
+
+app.all("/", (req, res) => {
+  res.status(200).send({
+    code: 200,
+    statustext: "OK",
+    success: true,
+    message: "API OF MIRЯOR APP",
+  });
+});
+
+app.all("*", (req, res) =>
+  res.send("SOMETHING THAT'S LOVED IS NEVER LOST. SORRY BUT NOW YOU'RE LOST.")
+);
+
+app.listen(process.env.PORT, () => {
+  console.log(`SERVER IS RUNNING ON PORT 1927 or ${process.env.PORT}`);
+});
