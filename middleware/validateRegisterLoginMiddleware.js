efafd56b7aca9347bbd07f724a3ab0b0820a61f4
frdnth <fardian.thofani@gmail.com>
2021-08-18 23:19:42 +0700

    adjusting validator

--- middleware/validateRegisterLoginMiddleware.js
+++ middleware/validateRegisterLoginMiddleware.js
@@ -75,20 +75,21 @@ exports.registerSchema = Joi.object({
 	}).unknown()
 
 exports.loginSchema = Joi.object({
-			username: Joi
-				.string()
-				.alphanum()
-				.min(4)
-				.max(20)
-				.required(),
-			email: Joi
-				.string()
-				.email()
-				.required(),
+			// username: Joi
+			// 	.string()
+			// 	.alphanum()
+			// 	.min(4)
+			// 	.max(20)
+			// 	.required(),
+			// email: Joi
+			// 	.string()
+			// 	.email()
+			// 	.required(),
 			password: Joi
 				.string()
 				.min(8)
 				.max(30)
 				// .pattern(/^[a-zA-Z0-9!@#$&()`.+,/"-]*$/)
 				.required(),
-		}).or('username', 'email')
+		}).unknown()
+		// .or('username', 'email')
